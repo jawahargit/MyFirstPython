7e8f098e4c3056da4f3bf3622a0639a6a334533b
jawahargit <87929156+jawahargit@users.noreply.github.com>
2022-09-10 18:28:35 +0000

    Initial commit

---
+++ My Python Journey/main.py
@@ -0,0 +1,38 @@
+#Shortcut to comment lines 
+# command + / in mac
+# ctrl + / in windows
+
+#Print Function will print to output here its console
+print('jawahar reddy')
+
+#Input function
+x = input('Enter your name:')
+print('Hello, ' + x)
+
+#Fundatemental data types 
+# int
+#Float
+
+#Type function will give Data type of the result after doing math 
+print(type(5+3.5))
+
+#This will give Remainder
+print(33%2)
+print(type(0.0))
+print(3//4)
+#This will give value before decimal in quotient
+print(type(3//4))
+print(50//100) 
+#This will give complete value in quotient
+print(50/100)
+print(type(50//100))
+print(type(50/100))
+
+print(12.5*3)
+#This will give 2 power 3
+print(2**3)
+
+
+#Math Functions
+print(round(45.678))
+
